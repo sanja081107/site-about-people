9ea6df4111bbd606daa44b1130b7b5150e36f2d2
new user <sanja081107@gmail.com>
2022-05-20 19:11:01 +0300

    add CBV

--- bestsite/people/urls.py
+++ bestsite/people/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='main'),
+    path('', HomeListView.as_view(), name='main'),
     path('about/', about, name='about'),
-    path('sign_in/', home, name='sign_in'),
-    path('add_article/', add_article, name='add_article'),
-    path('people/<slug:people_slug>/', people_detail, name='people_detail'),
-    path('category/<slug:cat_slug>/', category, name='category')
+    path('sign_in/', HomeListView.as_view(), name='sign_in'),
+    path('add_article/', PeopleCreateView.as_view(), name='add_article'),
+    path('people/<slug:people_slug>/', PeopleDetailView.as_view(), name='people_detail'),
+    path('category/<slug:cat_slug>/', CategoryListView.as_view(), name='category')
 
 ]
--- bestsite/people/views.py
+++ bestsite/people/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import *
 from .models import *
@@ -9,61 +11,120 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Page not found</h1>')
 
 
-def home(request):
-    posts = People.objects.all()
-    context = {
-        'posts': posts,
-        'title': 'People page',
-        'is_selected': 0,
-    }
-    return render(request, 'people/index.html', context)
-
-
 def about(request):
     context = {'title': 'About page'}
     return render(request, 'people/about.html', context)
 
+# ----------------------------------------------------------------
+class HomeListView(ListView):
+    model = People
+    template_name = 'people/index.html'
+    context_object_name = 'posts'
 
-def people_detail(request, people_slug):
-    post = get_object_or_404(People, slug=people_slug)
-    cat = post.cat.pk
-    context = {
-        'el': post,
-        'title': 'People detail page',
-        'is_selected': cat
-    }
-    return render(request, 'people/p_detail.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'People page'
+        context['is_selected'] = 0
+        return context
 
+    def get_queryset(self):
+        return People.objects.filter(is_published=True)
 
-def category(request, cat_slug):
-    cat = Category.objects.get(slug=cat_slug)
-    post = People.objects.filter(cat=cat)
-
-    if len(post) == 0:
-        raise Http404()
-
-    context = {
-        'posts': post,
-        'title': cat.name,
-        'is_selected': cat.pk
-    }
-    return render(request, 'people/index.html', context)
+# def home(request):
+#     posts = People.objects.all()
+#     context = {
+#         'posts': posts,
+#         'title': 'People page',
+#         'is_selected': 0,
+#     }
+#     return render(request, 'people/index.html', context)
 
 
-def add_article(request):
-    if request.method == 'POST':
-        form = PeopleForm(request.POST, request.FILES)
-        if form.is_valid():
-            try:
-                # People.objects.create(**form.cleaned_data)
-                form.save()
-                return redirect('main')
-            except:
-                form.add_error(None, 'Error data')  # Создается общая ошибка, если форма не связана с моделью и некорректна
-    else:
-        form = PeopleForm()
-    context = {
-        'form': form,
-        'title': 'Add article',
-    }
-    return render(request, 'people/add_article.html', context)
+# ----------------------------------------------------------------
+class PeopleDetailView(DetailView):
+    model = People
+    template_name = 'people/p_detail.html'
+    context_object_name = 'el'
+    slug_url_kwarg = 'people_slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = People.objects.get(slug=self.kwargs['people_slug'])
+        context['title'] = 'People detail page'
+        context['is_selected'] = post.cat.pk
+        return context
+
+# def people_detail(request, people_slug):
+#     post = get_object_or_404(People, slug=people_s    lug)
+#     cat = post.cat.pk
+#     context = {
+#         'el': post,
+#         'title': 'People detail page',
+#         'is_selected': cat
+#     }
+#     return render(request, 'people/p_detail.html', context)
+
+# ----------------------------------------------------------------
+class CategoryListView(ListView):
+    model = People
+    template_name = 'people/index.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        # cat = Category.objects.get(slug=self.kwargs['cat_slug'])
+        # post = People.objects.filter(cat=cat, is_published=True)
+        post = People.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)  # эту строку можно заменить 2мя предыдущими
+        if len(post) == 0:
+            raise Http404()
+        return post
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat = Category.objects.get(slug=self.kwargs['cat_slug'])
+        context['is_selected'] = cat.pk
+        context['title'] = cat.name
+        return context
+
+# def category(request, cat_slug):
+#     cat = Category.objects.get(slug=cat_slug)
+#     post = People.objects.filter(cat=cat)
+#
+#     if len(post) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': post,
+#         'title': cat.name,
+#         'is_selected': cat.pk
+#     }
+#     return render(request, 'people/index.html', context)
+# ----------------------------------------------------------------
+
+class PeopleCreateView(CreateView):
+    form_class = PeopleForm
+    template_name = 'people/add_article.html'
+    success_url = reverse_lazy('main')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Add article'
+        return context
+
+# def add_article(request):
+#     if request.method == 'POST':
+#         form = PeopleForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             try:
+#                 # People.objects.create(**form.cleaned_data)
+#                 form.save()
+#                 return redirect('main')
+#             except:
+#                 form.add_error(None, 'Error data')  # Создается общая ошибка, если форма не связана с моделью и некорректна
+#     else:
+#         form = PeopleForm()
+#     context = {
+#         'form': form,
+#         'title': 'Add article',
+#     }
+#     return render(request, 'people/add_article.html', context)
+# ----------------------------------------------------------------
